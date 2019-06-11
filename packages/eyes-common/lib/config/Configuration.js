b7d4750a7ccab2ac895511db29c51cdad063ade5
Yonatan Lehman <yonatan.lehman@applitools.com>
2019-06-11 12:08:38 +0300

    Add @signature/@sigparam for ConfigurationsetProperties to eliminate PropertyDataObject as agreed with Daniel

--- packages/eyes-common/lib/config/Configuration.js
+++ packages/eyes-common/lib/config/Configuration.js
@@ -391,6 +391,9 @@ class Configuration {
   }
 
   /**
+   * @signature `setProperties(propertyList)`
+   * @sigparam {PropertyData} propertyList - A list of name and value pairs
+   
    * @param {PropertyData[]|PropertyDataObject[]} value
    * @return {this}
    */
