9e22eee6b33213b59405f2096b70ac01ca41b592
Yonatan Lehman <yonatan.lehman@applitools.com>
2019-07-17 17:52:07 +0300

    Added @signature where required. Please confirm my understanding of the semantics in the comments - feel free to replace with what you want :-)

--- packages/eyes-images/lib/fluent/Target.js
+++ packages/eyes-images/lib/fluent/Target.js
@@ -7,6 +7,22 @@ const { ImagesCheckSettings } = require('./ImagesCheckSettings');
 
 class Target {
   /**
+   * @signature `image(Base64OrUrlorFilepath)`
+   * @sigparam {string} Base64OrUrlorPath - If the string is a Base64 image then it is used as the checkpoint image 
+   *       If the string matches the format of a URL then checkpoint image is taken from the URL
+   *       Otherwise the string is treated as the path to a file that contains an in image in png format.
+   *
+   * @signature `image(imageBuffer)`
+   * @sigparam {Buffer} imageBuffer - A Buffer object that contains an image that is used as the checkpoint image.
+   *
+   * @signature `image(imageProvider)`
+   * @sigparam {ImageProvider} imageProvider - An object provided by the user, it must provide a 
+   *  getImage() method that returns a MutableImage object (TBD or a Promise with a MutableImage ?)
+   *
+   * @signature `image(image)`
+   * @sigparam {MutableImage} mutableImage - An in memory image that is used as the checkpoint image
+   *      TBD - IS THIS FOR THE USER ?
+   *
    * @param {string|Buffer|ImageProvider|MutableImage} varArg
    * @return {ImagesCheckSettings}
    */
@@ -100,6 +116,21 @@ class Target {
   }
 
   /**
+   * @signature `image(Base64OrUrlorFilepath, rect)`
+   * @sigparam {string} Base64OrUrlorPath - If the string is a Base64 image then it is used as the checkpoint image 
+   *       If the string matches the format of a URL then checkpoint image is taken from the URL
+   *       Otherwise the string is treated as the path to a file that contains an in image in png format.
+   * @sigparam {Region} rect - A region within the image to be checked
+   *
+   * @signature `image(imageBuffer, rect)`
+   * @sigparam {Buffer} imageBuffer - A Buffer object that contains an image that is used as the checkpoint image.
+   * @sigparam {Region} rect - A region within the image to be checked
+   *
+   * @signature `image(image, rect)`
+   * @sigparam {MutableImage} mutableImage - An in memory image that is used as the checkpoint image
+   *      TBD - IS THIS FOR THE USER ?
+   * @sigparam {Region} rect - A region within the image to be checked
+   *
    * @param {string|Buffer|MutableImage} image
    * @param {Region|RegionObject} rect
    * @return {ImagesCheckSettings}
