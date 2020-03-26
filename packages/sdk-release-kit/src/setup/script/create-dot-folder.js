2845f5a5fd8e48b3fe8b2e700903bece7d41f3e9
David Haeffner <dhaeffner@gmail.com>
2020-03-26 10:14:23 +0200

    release-kit: added setup script to create dot-folder for the release-kit

--- packages/sdk-release-kit/src/setup/script/create-dot-folder.js
+++ packages/sdk-release-kit/src/setup/script/create-dot-folder.js
@@ -1,2 +1,10 @@
-// if .bongo dir exists return
-// else mkdir .bongo
+const fs = require('fs')
+const path = require('path')
+
+module.exports = packageDir => {
+  const workingDir = path.join(packageDir, '.bongo')
+  if (fs.statSync(workingDir).isDirectory()) {
+    process.exit(0)
+  }
+  fs.mkdirSync(workingDir)
+}
