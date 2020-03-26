cbe7010f0a3f394c4349014c35a6470cd670137a
David Haeffner <dhaeffner@gmail.com>
2020-03-26 10:13:54 +0200

    release-kit: added first pass on the pack & install script

--- packages/sdk-release-kit/src/dry-run/script/pack-install.js
+++ packages/sdk-release-kit/src/dry-run/script/pack-install.js
@@ -1,4 +1,17 @@
-// prepare dot-folder
-// prepare .bongo/dry-run dir (e.g., purge previous)
-// pack & mv the tar.gz to the dry-run dir
-// install pack
+const fs = require('fs')
+const path = require('path')
+const {exec} = require('child_process')
+const {promisify} = require('util')
+const pexec = promisify(exec)
+
+module.exports = async packageDir => {
+  const workingDir = path.join(packageDir, '.bongo', 'dry-run')
+  if (fs.statSync(workingDir).isDirectory()) {
+    // needs Node 12
+    fs.rmDirSync(workingDir, {recursive: true})
+  }
+  fs.mkdirSync(workingDir)
+  await pexec(`yarn pack --filename dry-run.tgz`)
+  await pexec(`npm install dry-run.tgz --prefix ${workingDir}`)
+  fs.unlinkSync('dry-run.tgz')
+}
