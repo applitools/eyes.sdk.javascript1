49a66a74ef1a9703e39317116b95ae63ac9a4d03
David Haeffner <dhaeffner@gmail.com>
2020-03-26 15:34:02 +0200

    release-kit: cleaned up setup

--- packages/sdk-release-kit/src/setup/scripts/create-dot-folder.js
+++ packages/sdk-release-kit/src/setup/scripts/create-dot-folder.js
@@ -3,9 +3,5 @@ const path = require('path')
 
 module.exports = packageDir => {
   const workingDir = path.join(packageDir, '.bongo')
-  try {
-    fs.statSync(workingDir)
-  } catch (error) {
-    fs.mkdirSync(workingDir)
-  }
+  if (!fs.existsSync(workingDir)) fs.mkdirSync(workingDir)
 }
