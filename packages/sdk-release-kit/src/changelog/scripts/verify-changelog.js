18d0f2c624617f11b7bed81c6189a06a734d06b5
David Haeffner <dhaeffner@gmail.com>
2020-03-29 12:07:47 +0300

    removed debugger statement

--- packages/sdk-release-kit/src/changelog/scripts/verify-changelog.js
+++ packages/sdk-release-kit/src/changelog/scripts/verify-changelog.js
@@ -3,7 +3,6 @@ const {verifyChangelog} = require('..')
 const path = require('path')
 
 module.exports = targetFolder => {
-  debugger
   const changelogContents = readFileSync(path.resolve(targetFolder, 'CHANGELOG.md'), 'utf8')
   const version = JSON.parse(readFileSync(path.resolve(targetFolder, 'package.json'), 'utf8'))
     .version
