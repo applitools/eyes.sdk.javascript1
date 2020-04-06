1ae39e008bf8638fcc059b77e14e23ab5133dfe1
David Haeffner <dhaeffner@gmail.com>
2020-04-06 20:44:43 +0300

    test-kit (coverage tests): initial plumbing to facilitate generating hook and command strings for a test for a given SDK implementation

---
+++ packages/sdk-test-kit/src/coverage-tests/code-export/index.js
@@ -0,0 +1,44 @@
+const {makeCoverageTests: doMakeCoverageTests} = require('../tests')
+const {getNameFromObject} = require('../common-util')
+
+function convertExecutionModeToSuffix(executionMode) {
+  if (executionMode.useStrictName) return ''
+  switch (getNameFromObject(executionMode)) {
+    case 'isVisualGrid':
+      return '_VG'
+    case 'isScrollStitching':
+      return '_Scroll'
+    default:
+      return ''
+  }
+}
+
+function makeEmitTests(initializeSdkImplementation, makeCoverageTests = doMakeCoverageTests) {
+  let output = {}
+  function emitTests(supportedTests, {branchName = 'master', host} = {}) {
+    supportedTests.forEach(supportedTest => {
+      const sdkImplementation = initializeSdkImplementation()
+      // hooks
+      if (sdkImplementation._setup) {
+        sdkImplementation._setup({
+          baselineTestName: `${supportedTest.name}${convertExecutionModeToSuffix(
+            supportedTest.executionMode,
+          )}`,
+          branchName,
+          host,
+        })
+      }
+      if (sdkImplementation._cleanup) sdkImplementation._cleanup()
+      // test
+      makeCoverageTests(sdkImplementation)[supportedTest.name]()
+      // store
+      output[supportedTest.name] = sdkImplementation.out
+    })
+    return output
+  }
+  return {emitTests}
+}
+
+module.exports = {
+  makeEmitTests,
+}
---
+++ packages/sdk-test-kit/test/coverage-tests/code-export/index.spec.js
@@ -0,0 +1,57 @@
+const assert = require('assert')
+const {makeEmitTests} = require('../../../src/coverage-tests/code-export')
+
+const fakeSdkImplementation = () => {
+  let result = {}
+  return {
+    _setup: () => {
+      result.setup = 'setup'
+    },
+    _cleanup: () => {
+      result.cleanup = 'cleanup'
+    },
+    open: () => {
+      result.open = 'open'
+    },
+    checkElement: () => {
+      result.checkElement = 'checkElement'
+    },
+    checkWindow: () => {
+      result.checkWindow = 'checkWindow'
+    },
+    close: () => {
+      result.close = 'close'
+    },
+    out: result,
+  }
+}
+const fakeCoverageTests = ({open, checkElement, checkWindow, close}) => {
+  return {
+    'test-a': () => {
+      open()
+      checkWindow()
+      close()
+    },
+    'test-b': () => {
+      open()
+      checkElement()
+      close()
+    },
+  }
+}
+
+describe('Code Export', () => {
+  it('returns strings to be written to file', () => {
+    const {emitTests} = makeEmitTests(fakeSdkImplementation, fakeCoverageTests)
+    const supportedTests = [{name: 'test-a', executionMode: {}}]
+    assert.deepStrictEqual(emitTests(supportedTests), {
+      'test-a': {
+        setup: 'setup',
+        cleanup: 'cleanup',
+        open: 'open',
+        checkWindow: 'checkWindow',
+        close: 'close',
+      },
+    })
+  })
+})
