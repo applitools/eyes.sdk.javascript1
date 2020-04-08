424b918f1ae5aa36505b64919db084d013e469c2
David Haeffner <dhaeffner@gmail.com>
2020-04-08 12:04:10 +0300

    test-kit (ct): prepare test/coverage/generic folder

--- packages/sdk-test-kit/src/coverage-tests/code-export/save.js
+++ packages/sdk-test-kit/src/coverage-tests/code-export/save.js
@@ -1,13 +1,15 @@
-const {writeFileSync} = require('fs')
+const fs = require('fs')
 const path = require('path')
 const {createTestFileString} = require('./render')
 
 function createTestFiles(emittedTests, testFrameworkTemplate) {
   const targetDirectory = path.join(process.cwd(), 'test', 'coverage', 'generic')
+  fs.rmdirSync(targetDirectory, {recursive: true})
+  fs.mkdirSync(targetDirectory)
   emittedTests.forEach(test => {
     const payload = createTestFileString(test, testFrameworkTemplate)
     const filePath = path.resolve(targetDirectory, `${test.name}.spec.js`)
-    writeFileSync(filePath, payload)
+    fs.writeFileSync(filePath, payload)
   })
 }
 
