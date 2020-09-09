2fcc848b2783efd10d91e6b60244a94f3af29c32
tourdedave <dave.haeffner@applitools.com>
2020-09-09 12:43:14 -0400

    shared: cleaned up generateDockerComposeConfig

--- packages/sdk-shared/src/generate-docker-compose-config.js
+++ packages/sdk-shared/src/generate-docker-compose-config.js
@@ -2,23 +2,26 @@ const fs = require('fs')
 const path = require('path')
 
 function generateDockerComposeConfig({saveToDisk, platform = process.platform} = {}) {
-  let config = {}
-  config.version = '3.4'
-  config.services = {
-    chrome: {
-      image: 'selenium/standalone-chrome:3.141.59-20200515',
-      volumes: ['/dev/shm:/dev/shm'],
-      ...generateNetworkConfigForPlatform(platform),
-    },
-    firefox: {
-      image: 'selenium/standalone-firefox',
-      volumes: ['/dev/shm:/dev/shm'],
-      ports: ['4445:4444'],
+  const config = {
+    version: '3.4',
+    services: {
+      chrome: {
+        image: 'selenium/standalone-chrome:3.141.59-20200515',
+        volumes: ['/dev/shm:/dev/shm'],
+        ...generateNetworkConfigForPlatform(platform),
+      },
+      firefox: {
+        image: 'selenium/standalone-firefox',
+        volumes: ['/dev/shm:/dev/shm'],
+        ports: ['4445:4444'],
+      },
     },
   }
   const result = JSON.stringify(config)
-  if (!saveToDisk) return result
-  fs.writeFileSync(path.resolve(process.cwd(), 'docker-compose.yaml'), result)
+  if (saveToDisk) {
+    fs.writeFileSync(path.resolve(process.cwd(), 'docker-compose.yaml'), result)
+  }
+  return result
 }
 
 function generateNetworkConfigForPlatform(platform) {
