076f6833b7f2831f98c6ba5bad7f107daccd08ba
Tal Hayut <tal.hayut@applitools.com>
2020-10-21 11:50:46 +0300

    fix: removing setupServerConfig

--- packages/eyes-sdk-core/lib/close/closeBatch.js
+++ packages/eyes-sdk-core/lib/close/closeBatch.js
@@ -5,24 +5,13 @@ const Configuration = require('../config/Configuration')
 const Logger = require('../logging/Logger')
 const {presult} = require('../troubleshoot/utils')
 
-function setupServerConfig({serverUrl, apiKey, proxy}) {
-  const logger = new Logger(!!process.env.APPLITOOLS_SHOW_LOGS)
-  const configuration = new Configuration()
-  if (serverUrl) configuration.setServerUrl(serverUrl)
-  if (apiKey) configuration.setApiKey(apiKey)
-  if (proxy) configuration.setProxy(proxy)
-  return {configuration, logger}
-}
-
-async function closeBatch({
-  batchIds = [],
-  serverUrl,
-  apiKey = process.env.APPLITOOLS_API_KEY,
-  proxy,
-}) {
-  const getAgentId = () => ''
-  const {logger, configuration} = setupServerConfig({serverUrl, apiKey, proxy})
-  const serverConnector = new ServerConnector({logger, configuration, getAgentId})
+async function closeBatch({batchIds = [], serverUrl, apiKey, proxy}) {
+  const serverConnector = new ServerConnector({
+    logger: new Logger(!!process.env.APPLITOOLS_SHOW_LOGS),
+    configuration: new Configuration({serverUrl, apiKey}),
+    proxy,
+    getAgentId: () => '',
+  })
 
   const promises = batchIds.map(batchId => serverConnector.deleteBatchSessions(batchId))
   const results = await Promise.all(promises.map(presult))
