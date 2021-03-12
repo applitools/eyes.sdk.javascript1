13dae52917aebeff1b713e4ed957d0ba20e52130
tourdedave <dave.haeffner@applitools.com>
2021-03-12 08:19:08 -0500

    shared: add enablePatterns test that checks passing the option in checkSettings

--- packages/sdk-shared/coverage-tests/custom/EnablePatterns.spec.js
+++ packages/sdk-shared/coverage-tests/custom/EnablePatterns.spec.js
@@ -6,21 +6,30 @@ const cwd = process.cwd()
 const spec = require(path.resolve(cwd, 'src/spec-driver'))
 
 describe('EnablePatterns', () => {
-  let driver, destroyDriver, eyes
+  let driver, destroyDriver
   before(async () => {
     ;[driver, destroyDriver] = await spec.build({browser: 'chrome'})
-    eyes = testSetup.getEyes({vg: true})
   })
   after(async () => {
     await destroyDriver()
   })
-  it('gets passed to Eyes', async () => {
+  it('in config', async () => {
     await spec.visit(driver, 'https://applitools.com/helloworld/')
-    await eyes.open(driver, 'eyes-testcafe', 'enablePatterns')
+    const eyes = testSetup.getEyes({vg: true})
     const config = eyes.getConfiguration()
     config.setEnablePatterns(true)
     eyes.setConfiguration(config)
+    await eyes.open(driver, 'eyes-testcafe', 'enablePatterns')
     await eyes.checkWindow('asdf')
+    const result = await eyes.close(false)
+    const testInfo = await getTestInfo(result)
+    assert.ok(testInfo['actualAppOutput']['0']['imageMatchSettings']['enablePatterns'])
+  })
+  it('in check settings', async () => {
+    await spec.visit(driver, 'https://applitools.com/helloworld/')
+    const eyes = testSetup.getEyes({vg: true})
+    await eyes.open(driver, 'eyes-testcafe', 'enablePatterns')
+    await eyes.check({enablePatterns: true})
     const result = await eyes.close(false)
     const testInfo = await getTestInfo(result)
     console.log(testInfo['actualAppOutput']['0'])
