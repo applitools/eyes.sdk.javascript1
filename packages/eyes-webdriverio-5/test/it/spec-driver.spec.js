53a27acf308724ad83268563480614e4add3df34
Tal Hayut <tal.hayut@applitools.com>
2021-07-08 11:54:34 +0300

    fix: fix spec-driver tests for mobile

--- packages/eyes-webdriverio-5/test/it/spec-driver.spec.js
+++ packages/eyes-webdriverio-5/test/it/spec-driver.spec.js
@@ -194,8 +194,23 @@ describe('spec driver', async () => {
         },
       })
     })
-    it.skip('getCookies()', async () => {
-      await getCookies(true)
+    it('getCookies()', async () => {
+      await spec.visit(browser, url)
+      await getCookies(true, {
+        all: false,
+        cookies: [
+          {
+            domain: undefined,
+            expiry: undefined,
+            sameSite: undefined,
+            httpOnly: false,
+            path: '/',
+            secure: true,
+            name: 'hello',
+            value: 'world',
+          },
+        ],
+      })
     })
   })
 
@@ -231,8 +246,23 @@ describe('spec driver', async () => {
         },
       })
     })
-    it.skip('getCookies()', async () => {
-      await getCookies(true)
+    it('getCookies()', async () => {
+      await spec.visit(browser, url)
+      await getCookies(true, {
+        all: false,
+        cookies: [
+          {
+            domain: 'applitools.github.io',
+            expiry: undefined,
+            sameSite: undefined,
+            httpOnly: false,
+            path: '/',
+            secure: true,
+            name: 'hello',
+            value: 'world',
+          },
+        ],
+      })
     })
   })
 
@@ -544,7 +574,7 @@ describe('spec driver', async () => {
       expected,
     )
   }
-  async function getCookies(legacy = false) {
+  async function getCookies(legacy = false, expected) {
     if (!legacy) {
       const cdpCommand = [
         'Network.setCookie',
@@ -587,12 +617,8 @@ describe('spec driver', async () => {
         ],
       })
     } else {
-      // TODO: implement IE test
-
-      await browser.addCookie({name: 'hello', value: 'world'})
-      assert.deepStrictEqual(await spec.getCookies(browser), [
-        {name: 'hello', value: 'world', domain: 'applitools.github.io'},
-      ])
+      await browser.addCookie({name: 'hello', value: 'world', secure: true})
+      assert.deepStrictEqual(await spec.getCookies(browser), expected)
     }
   }
 })
